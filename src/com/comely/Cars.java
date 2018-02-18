9ad80ea062b3cf39624368377162e9ff652add99
Administrator <Administrator@lixiaodong>
2018-02-18 11:34:22 +0800

    chy

---
+++ src/com/comely/Cars.java
@@ -0,0 +1,9 @@
+package com.comely;
+
+public class Cars {
+	public int num;
+	public String colour;
+	public void run(){
+		System.out.println(num+",,,,,,,,"+colour);
+	}
+}
---
+++ src/com/lixiaodong/Car.java
@@ -0,0 +1,15 @@
+package com.lixiaodong;
+
+import com.comely.Cars;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cars c=new Cars();
+		c.num=4;
+		c.colour="read";
+		c.run();
+	}
+
+}
